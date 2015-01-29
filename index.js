efe920ecdf5eae3a8f7b23b3b92c1de8da1ba864
Xav Laumonier <xlaumoni@cisco.com>
2015-01-29 12:48:42 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+var Server = require('socket.io');
+var io = new Server();
+
+io.on('connection', function(socket){
+    console.log('New connection');
+});
+
+exports.reload = function() {
+    io.emit('reload');
+};
+
+exports.init = function(port) {
+    io.attach(port || 1234);
+};
